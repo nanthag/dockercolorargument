92c775625cc594968decf3dfdf29be74365d0be9
nanthags <nanthags@gmail.com>
2023-05-31 10:00:49 +0530

    Add project files.

---
+++ dockercolorargument/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dockercolorargument.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<string> env = new List<string>();
+        public List<string> args = new List<string>();
+        public string color {  get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var envlist = Environment.GetEnvironmentVariables();
+
+            foreach(var key in envlist.Keys)
+            {
+                env.Add(envlist[key].ToString());
+            }
+
+            color = Environment.GetEnvironmentVariable("COLOR");
+
+            args = Environment.GetCommandLineArgs().ToList();
+
+
+        }
+    }
+}
